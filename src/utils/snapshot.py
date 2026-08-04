c48821810d0b3736899971c9ca768447aee7aace
GYM <gym753357@qq.com>
2026-08-04 14:43:22 +0800

    feat: 添加 recovery.py 对 GuardianRecovery 环境快照的管理，重构快照创建逻辑

--- src/utils/snapshot.py
+++ src/utils/snapshot.py
@@ -13,14 +13,27 @@ from utils.process import Process
 class Snapshot:
     def __init__(self,db):
         self.db = db
-        self.snapshot_dir = os.path.join(Exec.get_exe_path(),'data','snapshot')
+        self.snapshot_path = os.path.join(Exec.get_exe_path(),'data','snapshot')
+        self.recovery_snapshot_path = os.path.join(os.environ.get('SystemDrive', 'C:') + '\\', 'GuardianRecovery', 'data','snapshot')
         self.Process = Process(db)
         self.classisland_path = db.path.get('classisland_path')
-        
+
+    def _zip_dir(self, src, dst):
+        '压缩指定文件夹文件到指定路径。 成功返回 True ，失败返回 False'
+        try:
+            with zipfile.ZipFile(dst, 'w', zipfile.ZIP_DEFLATED) as zf:
+                for root, _, files in os.walk(src):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        arcname = os.path.relpath(file_path, self.classisland_path)
+                        zf.write(file_path, arcname)
+        except Exception as e:
+            Log.error(f'压缩文件时出错，错误是：{e}')
+    
     def list_snapshot(self):
         '列出所有可用的快照。 成功返回列表，失败返回False'
         try:
-            filelist = os.listdir(self.snapshot_dir)
+            filelist = os.listdir(self.snapshot_path)
             filelist.sort(reverse=True)
             return filelist
         except Exception as e:
@@ -30,14 +43,14 @@ class Snapshot:
     def restore_snapshot(self,name):
         '恢复到指定的快照。 传入要恢复快照的文件名(string) 成功返回True，失败返回False'
         Exec.kill_process(self.db.path.get('classisland_process_name'))
-        if os.path.exists(os.path.join(self.snapshot_dir,name)):
+        if os.path.exists(os.path.join(self.snapshot_path,name)):
             try:
                 try:
                     shutil.rmtree(self.classisland_path)
                 except:
                     pass
                 os.mkdir(self.classisland_path)
-                with zipfile.ZipFile(os.path.join(self.snapshot_dir,name), 'r') as zf:
+                with zipfile.ZipFile(os.path.join(self.snapshot_path,name), 'r') as zf:
                     zf.extractall(path=self.classisland_path)
                     Log.info(f'成功恢复到指定快照：{name}')
                     return True
@@ -58,16 +71,14 @@ class Snapshot:
 
         try:
             if os.path.exists(self.classisland_path):
-                os.makedirs(self.snapshot_dir, exist_ok=True)
-                with zipfile.ZipFile(os.path.join(self.snapshot_dir,zip_name), 'w', zipfile.ZIP_DEFLATED) as zf:
-                    for root, _, files in os.walk(self.classisland_path):
-                        for file in files:
-                            file_path = os.path.join(root, file)
-                            arcname = os.path.relpath(file_path, self.classisland_path)
-                            zf.write(file_path, arcname)
+                os.makedirs(self.snapshot_path, exist_ok=True)
+                if(not self._zip_dir(self.classisland_path, os.path.join(self.snapshot_path, zip_name))):
+                    return False
+                os.makedirs(self.recovery_snapshot_path, exist_ok=True)
+                if(not self._zip_dir(self.classisland_path, os.path.join(self.recovery_snapshot_path, zip_name))):
+                    return False
                 return zip_name
             else:
-                Log.error(f'压缩文件时出错，错误是：ClassIsland目录不存在')
                 return False
         except Exception as e:
             Log.error(f'压缩文件时出错，错误是：{e}')
@@ -76,7 +87,8 @@ class Snapshot:
     def remove_snapshot(self,name):
         '移除指定的快照。 传入要删除的快照名称(String) 成功返回True，失败返回False'
         try:
-            os.remove(os.path.join(self.snapshot_dir, name))
+            os.remove(os.path.join(self.snapshot_path, name))
+            os.remove(os.path.join(self.recovery_snapshot_path, name))
             return True
         except Exception as e:
             Log.error(f'移除快照时出错，错误为：{e}')
